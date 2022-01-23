0e30a855d892eee8d389437f291217bec5625ee0
Arturo Mata <81439664+matarturo@users.noreply.github.com>
2022-01-23 15:38:29 -0400

    Update mapper.sh
    Se definieron los puertos a evaluar y opciones del escaneo en la variable $vuln

--- mapper.sh
+++ mapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script: mapper.sh
 # Creado por: Arturo Mata < arturo.mata@gmail.com >
-# Versión: 1.0.0
+# Versión: 1.0.1
 # DESCARGO DE RESPONSABILIDAD
 # Este programa está diseñado con fines de investigación busqueda de vulnerabilidades en la configuracion de equipos,
 # y el autor no asumirá ninguna responsabilidad si se le da cualquier otro uso.
@@ -12,17 +12,18 @@
 #  * Thorough Scan
 
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 subnet" >&2
+    echo "Usar el comando: $0 <IP> o <subnet>" >&2
     exit 1
 fi
 
 # Definir puertos a evaluar y opciones del escaneo
 opts="-T4 --open"
 pingopts="-sn -PS21-23,25,53,80,443,445,3389,8333,9001 -PO -PE -PM -PP -PN"
+vuln="-sS -Pn -T4 --script vuln"
 
 # Uso de funciones NMAP para detectar host activos
 echo "--------"
-echo "Buscando hosts activos"
+echo "Buscando hosts activos..."
 echo "--------"
 echo "nmap $opts $pingopts -oG alive.gnmap $1"
 nmap $opts $pingopts -oG alive.gnmap $1
@@ -34,12 +35,16 @@ echo "[+] Encontrados un total de $count hosts activos."
 # Buscar puertos abiertos o vulnerables a virus en un segmento de red
 echo ""
 echo "--------"
-echo "Buscando puertos abiertos"
+echo "Buscando vulnerabilidades..."
 echo "--------"
-echo "nmap $opts -iL targets -p 1-4910 -oG ports.gnmap"
-nmap $opts -iL targets -p 1-9001 -oG ports.gnmap
+echo "nmap $vuln -iL targets"
+nmap $vuln -iL targets
+
+# echo "nmap $opts -iL targets -p 1-9110 -oG ports.gnmap"
+# nmap $opts -iL targets -p 1-9001 -oG ports.gnmap
 
 # Recopila datos sobre los distintos tipos de puertos detectados abiertos en un segmento de red
 grep -o -E "[0-9]+/open" ports.gnmap | cut -d "/" -f1 | sort -u > ports
 count=$(wc -l ports | awk '{ print $1 }')
 echo "[+] Encontrados un total de $count tipos puertos abiertos"
+exit 0
